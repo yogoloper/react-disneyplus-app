d3819eafc7d775bff0457825f0dae25ee36da5c7
yogoloper <jguddnjs@gmail.com>
2024-02-16 22:57:18 +0900

    이미지 배너 생성 2

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../api/axios'
 import requests from '../api/request'
+import './Banner.css'
 
 const Banner = () => {
   const [movie, setMovie] = useState([])
@@ -23,10 +24,37 @@ const Banner = () => {
     setMovie(movieDetail);
   }
   
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + '...' : str;
+  }
+
   return (
-    <div>
-      
-    </div>
+    <header
+      className='banner'
+      style={{
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie.backdrop_path}')`,
+        backgroundPosition: 'top center',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className='banner__contents'>
+        <h1 className='banner__title'>
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+        <div className='banner__buttons'>
+          {movie?.videos?.results[0]?.key && 
+            <button className='banner__button play'>
+              Play
+            </button>
+          }
+        </div>
+        <p className='banner__description'>
+          {truncate(movie.overview, 100)}
+        </p>
+      </div>
+      <div className='banner__fadeBottom'>
+      </div>
+    </header>
   )
 }
 
