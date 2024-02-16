2dee4259a83dc0c8b0ef7eb293e8b92658cd4cb3
yogoloper <jguddnjs@gmail.com>
2024-02-16 22:25:52 +0900

    기본 배경 및 이미지 배너 생성

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from '../api/axios'
+import requests from '../api/request'
 
 const Banner = () => {
+  const [movie, setMovie] = useState([])
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    const response = await axios.get(requests.fetchNowPlaying);
+
+    const movieId = response.data.results[
+      Math.floor(Math.random() * response.data.results.length)
+    ].id
+
+    const {data: movieDetail} = await axios.get( `movie/${movieId}`, {
+      params: {
+        append_to_response: 'videos',
+      }
+    });
+    setMovie(movieDetail);
+  }
+  
   return (
     <div>
       
