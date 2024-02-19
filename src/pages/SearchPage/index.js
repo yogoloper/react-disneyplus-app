3d264a1fe4757c7b0110e4f134869d4b20e64143
yogoloper <jguddnjs@gmail.com>
2024-02-19 21:54:51 +0900

    검색 페이지 css 추가

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,7 @@
 import axios from '../../api/axios';
 import React, { useEffect, useState } from 'react'
-import { unstable_useViewTransitionState, useLocation } from 'react-router-dom'
+import {useLocation, useNavigate } from 'react-router-dom'
+import './SearchPage.css'
 
 function SearchPage() {
   const [searchResults, setSearchResults] = useState([])
@@ -11,6 +12,7 @@ function SearchPage() {
 
   let query = useQuery();
   const searchTerm = query.get('q');
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (searchTerm) {
@@ -18,23 +20,48 @@ function SearchPage() {
     }
   }, [searchTerm])
   
-
-const fetchSearchMovie = async (searchTerm) => {
-  try {
-    const response = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
-    setSearchResults(response.data.results);
-  } catch (err) {
-    console.log(err);
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const response = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
+      setSearchResults(response.data.results);
+    } catch (err) {
+      console.log(err);
+    }
   }
-}
 
-
-
-  return (
-    <div>
-      Search Page
-    </div>
-  )
+  if (searchResults.length > 0) {
+    return (
+      <section className='search-container'>
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== 'person') {
+            const movieImageUrl = 'https://image.tmdb.org/t/p/w500' + movie.backdrop_path
+            return (
+              <div className='movie' key={movie.id}>
+                <div className='movie__column-poster'>
+                  <img
+                    src={movieImageUrl} alt='movie' className='movie__poster'
+                    onClick={() => {
+                      navigate(`/${movie.id}`)
+                    }}
+                  />
+                </div>
+              </div>
+            )
+          }
+        })}
+      </section>
+    )
+  } else {
+    return (
+      <section className='no-results'>
+        <div className='no-results-text'>
+          <p>
+            찾고자하는 검새어 `${searchTerm}` 에 맞는 영화가 없습니다.
+          </p>
+        </div>
+      </section>
+    )
+  }
 }
 
 export default SearchPage
