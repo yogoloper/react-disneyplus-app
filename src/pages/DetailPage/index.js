24d2878eb1c5b23b8bf19ca1650ca5fb484f5b06
yogoloper <jguddnjs@gmail.com>
2024-02-19 22:18:50 +0900

    useParams 사용하여 상세페이지 생성

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useParams } from 'react-router-dom';
+import axios from '../../api/axios';
+import React, { useEffect, useState } from 'react'
 
 function DetailPage() {
-  return (
-    <div>
-      Detail Page
-    </div>
-  )
+  let { movieId } = useParams();
+  const [movie, setMovie] = useState({});
+  
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`/movie/${movieId}`);
+      setMovie(response.data);
+    }
+    fetchData();
+  }, [movieId])
+
+  if (movie) {
+    return (
+      <section>
+        <img
+          className='modal__poster-img'
+          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+          alt='img'
+        />
+      </section>
+    )
+  } else {
+    return null;
+  }
 }
 
 export default DetailPage
