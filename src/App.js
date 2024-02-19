aa7475ab1c79f631baf6d82f439393ab75ef0217
yogoloper <jguddnjs@gmail.com>
2024-02-19 19:12:37 +0900

    react router dom  적용

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
-import styled from 'styled-components';
 import './App.css';
 import Nav from './components/Nav';
-import Banner from './components/Banner';
-import Category from './components/Category';
-import Row from './components/Row';
-import requests from './api/request';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage';
+import SearchPage from './pages/SearchPage';
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+    </div>
+  )
+}
 
 function App() {
   return (
-    <Container>
-      <Nav />
-      <Banner />
-      <Category />
-      <Row title='Trending Now' id='TN' fetchUrl={requests.fetchTrending}/>
-      <Row title='Top Rated' id='TR' fetchUrl={requests.fetchTopRated}/>
-      <Row title='Action Movies' id='AM' fetchUrl={requests.fetchActionMovies}/>
-      <Row title='Comedy Movies' id='CM' fetchUrl={requests.fetchComedyMovies}/>
-    </Container>
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Layout />} />
+          <Route index element={<LoginPage />} />
+          <Route path='main' element={<MainPage />} />
+          <Route path=':movieId' element={<DetailPage />} />
+          <Route path='search' element={<SearchPage />} />
+      </Routes>
+    </div>
   );
 }
 
 export default App;
-
-const Container = styled.main`
-  position: relative;
-  min-height: calc(100vh - 250px);  
-  overflow: hidden;
-  display: block;
-  top: 72px;
-  padding: 0 calc(3.5ww + 5px);
-
-  &::after { 
-    background: url('images/home-background.png') center center / cover no-repeat fixed;
-    content: '';
-    position: absolute;
-    inset: 0px;
-    opacity: 1;
-    z-index: -1;
-`;
